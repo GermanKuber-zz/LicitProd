f8f8f840aeca301b3b2e3d68b41868184a3b085c
Germán Küber <german.kuber@outlook.com>
2019-09-04 09:30:13 -0300

    Delete ChangeLanguage from the class

--- LicitProd.UI/MainContainer.cs
+++ LicitProd.UI/MainContainer.cs
@@ -24,25 +24,9 @@ namespace LicitProd.UI
         protected override void OnMdiChildActivate(EventArgs e)
         {
             base.OnMdiChildActivate(e);
-            TranslationService.GetTranslation()
-                         .Success(x => ChangeLanguage(x));
         }
 
-        public void ChangeLanguage(Translations translation)
-        {
-            translation._translations.ForEach(x =>
-            {
-                var controls = this.Controls.Find(x.Key, true);
-                foreach (var control in controls)
-                {
-                    if (control is Label)
-                        control.Text = x.Value;
-                    if (control is Button)
-                        control.Text = x.Value;
-                }
-            });
-        }
-
+      
 
         private void LogoutToolStripMenuItem_Click(object sender, EventArgs e)
         {
